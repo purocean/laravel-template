5da07124009677108086dc1933bdba14c4dc4009
Yang <purocean@gmail.com>
2017-03-16 18:35:22 +0800

    补上接口测试

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -24,19 +24,49 @@ class UserTest extends TestCase
 
     public function testSync()
     {
-
         $this->postJson('/api/users/sync')->assertStatus(401);
 
         $this->iam('usertest')->postJson('/api/users/sync')->assertStatus(403);
 
-        $this->iam('suadmin')->getJson('/api/users/sync')->assertStatus(405);
+        $this->iam('useradmin')->getJson('/api/users/sync')->assertStatus(405);
 
-        $this->iam('suadmin', function () {
+        $this->iam('useradmin', function () {
             Queue::fake();
         })->postJson('/api/users/sync')
             ->assertStatus(200)
             ->assertJson(['status' => 'ok']);
 
         Queue::assertPushed(SyncUserFromQywx::class);
+    }
+
+    public function testAllroles()
+    {
+        $this->getJson('/api/users/allroles')->assertStatus(401);
+
+        $this->iam('usertest')->getJson('/api/users/allroles')->assertStatus(403);
+
+        $this->iam('useradmin')
+            ->getJson('/api/users/allroles')
+            ->assertStatus(200)
+            ->assertJson(['status' => 'ok']);
+    }
+
+    public function testAttachroles()
+    {
+        $this->postJson('/api/users/attachroles')->assertStatus(401);
+
+        $this->iam('usertest')->postJson('/api/users/attachroles')->assertStatus(403);
+
+        $this->iam('useradmin')->getJson('/api/users/attachroles')->assertStatus(405);
+
+        $this->iam('useradmin')
+            ->postJson('/api/users/attachroles', ['username' => 'usertest', 'rolenames' => ['admin', 'xxxx']])
+            ->assertStatus(200)
+            ->assertJson(['status' => 'error']);
+
+        $this->iam('useradmin')
+            ->postJson('/api/users/attachroles', ['username' => 'usertest', 'rolenames' => ['admin', 'user']])
+            ->assertStatus(200)
+            ->assertJson(['status' => 'ok']);
     }
 }
