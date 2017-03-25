be26fe55e0f743f4beae1a6bcaf649387645da6c
Yang <purocean@gmail.com>
2017-03-25 17:54:52 +0800

    Http增加文件下载

--- frontend/src/utils/Http.js
+++ frontend/src/utils/Http.js
@@ -12,49 +12,72 @@ const fixFullUrl = url => {
   return url
 }
 
-export default {
-  fixFullUrl,
+const fetchWithAuth = (url, params = {}, cbSuccess = (() => {}), cbError = (() => {}), ...other) => {
+  params = Object.assign({
+    headers: Object.assign({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + Auth.getAccessToken()
+    }, params.headers),
+  }, params, {
+    body: typeof params.body === 'object' ? JSON.stringify(params.body) : params.body,
+  })
 
-  fetch: (url, params = {}, cbSuccess = (() => {}), cbError = (() => {}), ...other) => {
-    params = Object.assign({
-      headers: Object.assign({
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + Auth.getAccessToken()
-      }, params.headers),
-    }, params, {
-      body: typeof params.body === 'object' ? JSON.stringify(params.body) : params.body,
-    })
+  return fetch(url, params, ...other).then(response => {
+    let token = response.headers.get('Authorization')
+    if (token) {
+      Auth.setAccessToken(token)
+    }
 
-    return fetch(url, params, ...other).then(response => {
-      let token = response.headers.get('Authorization')
-      if (token) {
-        Auth.setAccessToken(token)
-      }
-
-      if (response.ok) {
-        response.json().then((data) => cbSuccess(data, response))
+    if (response.ok) {
+      if (response.headers.get('Content-Type') === 'application/json') {
+        response.json().then(data => cbSuccess(data, response))
       } else {
-        if (response.status === 401) {
-          Auth.setAccessToken('')
-          Auth.setPermissions({})
-          Auth.setRoles({})
-          Auth.setUser('')
-          location.reload()
-        } else if (response.status === 403) {
-          Auth.setPermissions({})
-          Auth.setRoles({})
-          alert('无权限访问资源')
-        } else {
-          alert(`Error: ${response.status}`)
-        }
-
-        cbError(response)
-        console.log('Network response was not ok.')
+        cbSuccess(response.body, response)
       }
-    }).catch(error => {
-      cbError(error)
-      console.log('There has been a problem with your fetch operation: ' + error.message)
-    })
-  }
+    } else {
+      if (response.status === 401) {
+        Auth.setAccessToken('')
+        Auth.setPermissions({})
+        Auth.setRoles({})
+        Auth.setUser('')
+        location.reload()
+      } else if (response.status === 403) {
+        Auth.setPermissions({})
+        Auth.setRoles({})
+        alert('无权限访问资源')
+      } else {
+        alert(`Error: ${response.status}`)
+      }
+
+      cbError(response)
+      console.log('Network response was not ok.')
+    }
+  }).catch(error => {
+    cbError(error)
+    console.log('There has been a problem with your fetch operation: ' + error.message)
+  })
 }
+
+const download = (url, params = {}) => {
+  fetchWithAuth(url, params, (body, response) => {
+    response.blob().then(data => {
+      let fileName = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/
+                      .exec(response.headers.get('Content-Disposition'))[1] || 'download'
+
+      console.log(fileName)
+
+      if (window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(data, fileName)
+      } else {
+        let link = document.createElement('a')
+        link.href = window.URL.createObjectURL(data, {type: response.headers.get('Content-Type')})
+        link.download = fileName
+        link.click()
+        window.URL.revokeObjectURL(link.href)
+      }
+    })
+  })
+}
+
+export default { fixFullUrl, fetch: fetchWithAuth, download }
