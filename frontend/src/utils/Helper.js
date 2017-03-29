c774eca8423595f0a3787d628129237e96a41512
Yang <purocean@gmail.com>
2017-03-29 17:49:57 +0800

    增加容错

--- frontend/src/utils/Helper.js
+++ frontend/src/utils/Helper.js
@@ -1,7 +1,11 @@
 const textTree = (data, level = 1) => {
+  if (!data || data.length === 0) {
+    return ''
+  }
+
   return data.map(item => {
     let padding = new Array(level + 1).join('  ') +
-        (item.children && item.children.length > 1 ? '+ ' : '- ')
+        (item.children && item.children.length > 0 ? '+ ' : '- ')
     return `${padding}${item.title}\n` + textTree(item.children, level + 1)
   }).join('')
 }
